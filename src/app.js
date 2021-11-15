201dcf441e588b59cfcf0cb1e02679669f8c9a62
LJKR2020 <loredana.rietveld@gmail.com>
2021-11-15 22:48:12 +0100

    add function currencies

--- src/app.js
+++ src/app.js
@@ -17,21 +17,53 @@ const search = (e) => {
 
 form.addEventListener('submit', search);
 
+const currencies = (currencies) => {
+    if (currencies.length > 1) {
+        let curString = '';
+        for (i = 0; i < currencies.length; i++) {
+            if (currencies.length - 1 == i) {
+                curString += currencies[i].name;
+            } else {
+                curString +=  `${currencies[i].name} and `;
+            }
+        }
+        return curString;
+    }
+
+    return `${currencies[0].name}'s`
+};
+
 const generateResults = (data) => {
     data.forEach((country) => {
         const el = document.createElement('div');
-        el.innerText = country.translations.nl;
-        results.appendChild(el)
+        const img = document.createElement('img');
+        img.src = country.flag;
+        el.appendChild(img);
+
+        const title = document.createElement('h2');
+        title.textContent = country.name;
+        el.appendChild(title);
+
+        const description = document.createElement('p');
+        el.appendChild(description);
+        description.textContent = `${country.translations.nl} is situated in ${country.subregion}.
+      It has a population of ${country.population} people.
+      The capital is ${country.capital} and you can pay with ${currencies(country.currencies)}`;
+
+        results.appendChild(el);
     });
 }
 
-const getCountriesByName = () => {
-    axios.get(`${api}/name/${query.value}`)
+const getCountriesByName = (term = query.value) => {
+    axios.get(`${api}/name/${term}`)
         .then(res => {
             console.error(res);
 
             results.innerHTML = '';
             generateResults(res.data);
+        })
+        .finally(() => {
+            query.value = '';
         });
 }
 
@@ -44,4 +76,5 @@ const getAllCountries = () => {
         });
 }
 
-getAllCountries()
+// getAllCountries();
+getCountriesByName();
